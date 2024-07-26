9a908824af9a36f81c84b2a5ff6c542fbd0a6d92
btomka <boris.tomka@hp.com>
2024-07-26 15:43:14 +0200

    more tune

--- pull_data.py
+++ pull_data.py
@@ -56,11 +56,11 @@ def logout(api_ip, session_id):
         print("OOps: Something Else", err)
 
 # Function to grab data
-def grab_data(api_ip, session_id, interval, duration, ids):
+def grab_data(api_ip, session_id, interval, duration):
     try:
         kpi_url = f"https://{api_ip}/rest/kpi"
         payload = {
-            "ids": ids
+            "ids": ["SYSTEM_NAME", "MEM_STATS"]
         }
         headers = {
             "accept": "application/json",
@@ -98,13 +98,13 @@ if __name__ == "__main__":
     PASSWORD = os.getenv("API_PASSWORD", "admin")
     INTERVAL = int(os.getenv("API_INTERVAL", 5))
     DURATION = int(os.getenv("API_DURATION", 30))
-    IDS = os.getenv("API_IDS", ["SYSTEM_NAME", "MEM_STATS"])
+    #IDS = os.getenv("API_IDS", ["SYSTEM_NAME", "MEM_STATS"])
     
     # Suppress SSL warnings for self-signed certificates
     requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
     
     try:
         session_id = login(API_IP, USERNAME, PASSWORD)
-        grab_data(API_IP, session_id, INTERVAL, DURATION, IDS)
+        grab_data(API_IP, session_id, INTERVAL, DURATION)
     finally:
         logout(API_IP, session_id)
